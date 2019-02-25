9195b3d1e3ea9dd9cadac2563643175da127d839
arley <34805850@qq.com>
2019-02-25 20:42:12 +0800

    3.html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development', // 模式 production development
   entry: './src/index.js', // 入口
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js' // 打包后的文件名
+    filename: 'bundle.[hash:8].js' // 打包后的文件名
   },
   devServer: { //开发服务器的配置
     port: 1997,
@@ -15,5 +16,16 @@ module.exports = {
     contentBase: './dist',
     compress: true, // 启用压缩
     open: true // 自动打开浏览器
-  }
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html', // 打包模板
+      filename: 'index.html', // 输出的文件
+      minify: {
+        removeAttributeQuotes: true, //删除双引号
+        collapseWhitespace: true // 压缩成一行
+      },
+      hash: true
+    })
+  ]
 };
