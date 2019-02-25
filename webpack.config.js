562d24528e3371f10521419bc2c6df6115871d93
arley <34805850@qq.com>
2019-02-25 20:33:03 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,12 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js' // 打包后的文件名
+  },
+  devServer: { //开发服务器的配置
+    port: 1997,
+    progress: true,
+    contentBase: './dist',
+    compress: true, // 启用压缩
+    open: true // 自动打开浏览器
   }
 };
